72a98ebd20ec48c007c754ed5ba04b0342beabf5
JamesA <G00379271@atu.ie>
2023-10-18 12:56:56 +0100

    Add interface to talk to the next microservice we add.
    Add hashmap to store responseMessage to pass on to the next microservice

--- src/main/java/ie/atu/RegistrationController.java
+++ src/main/java/ie/atu/RegistrationController.java
@@ -1,15 +1,32 @@
 package ie.atu;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class RegistrationController {
 
+
+    private RegistrationServiceClient registrationServiceClient;
+
+    @Autowired
+    public RegistrationController(RegistrationServiceClient registrationServiceClient) {
+        this.registrationServiceClient = registrationServiceClient;
+    }
+
     @PostMapping("/confirm")
-    public String regsiterUser(@RequestBody UserDetails uD) {
-        return String.format("Name: %s, Email: %s", uD.getName(), uD.getEmail());
+    public Map<String, String> registerUser (@RequestBody UserDetails userDetails) {
+
+        String register = registrationServiceClient.moreDetails(userDetails);
+        Map<String, String> responseMessage = new HashMap<>();
+        responseMessage.put("message", register);
+        return responseMessage;
+
     }
 
 }
---
+++ src/main/java/ie/atu/RegistrationServiceClient.java
@@ -0,0 +1,12 @@
+package ie.atu;
+
+import org.apache.catalina.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name="registration-service", url="http://localhost:8083")
+public interface RegistrationServiceClient {
+    @PostMapping("/registration")
+    String moreDetails(@RequestBody UserDetails userDetails);
+}
